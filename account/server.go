2f980b1afa05727ec5b5808a557e6aba517baec0
cipepser <respepic@gmail.com>
2018-09-07 08:02:09 +0900

    add server.go

---
+++ account/server.go
@@ -0,0 +1,68 @@
+//go:generate protoc ./account.proto --go_out=plugins=grpc:./pb
+package account
+
+import (
+	"fmt"
+	"net"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	"github.com/cipepser/graphql-microservice-sample/account/pb"
+)
+
+type grpcSsrver struct {
+	service Service
+}
+
+func ListenGRPC(s Service, port int) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return err
+	}
+	serv := grpc.NewServer()
+	pb.RegisterAccountServiceServer(serv, &grpcSsrver{s})
+	reflection.Register(serv)
+	return serv.Serve(lis)
+}
+
+func (s *grpcSsrver) PostAccount(ctx context.Context, r *pb.PostAccountRequst) (*pb.PostAccountResponse, error) {
+	a, err := s.service.PostAccount(ctx, r.Name)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.PostAccountResponse{Account: &pb.Account{
+		Id:   a.ID,
+		Name: a.Name,
+	}}, nil
+}
+
+func (s *grpcSsrver) GetAccount(ctx context.Context, r *pb.GetAccountRequest) (*pb.GetAccountResponse, error) {
+	a, err := s.service.GetAccount(ctx, r.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.GetAccountResponse{
+		Account: &pb.Account{
+			Id:   a.ID,
+			Name: a.Name,
+		},
+	}, nil
+}
+
+func (s *grpcSsrver) GetAccounts(ctx context.Context, r *pb.GetAccountsRequest) (*pb.GetAccountsResponse, error) {
+	res, err := s.service.GetAccounts(ctx, r.Skip, r.Take)
+	if err != nil {
+		return nil, err
+	}
+	accounts := []*pb.Account{}
+	for _, p := range res {
+		accounts = append(accounts,
+			&pb.Account{
+				Id:   p.ID,
+				Name: p.Name,
+			})
+	}
+	return &pb.GetAccountsResponse{Accounts: accounts}, nil
+}
