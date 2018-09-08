481b8dbd5b6de7ee4cbccf518dba5801a4ef020f
cipepser <respepic@gmail.com>
2018-09-08 13:24:25 +0900

    add server.go

---
+++ catalog/server.go
@@ -0,0 +1,87 @@
+//go:generate protoc ./catalog.proto --go_out=plugins=grpc:./pb
+package catalog
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"golang.org/x/net/context"
+
+	"github.com/cipepser/graphql-microservice-sample/catalog/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type grpcServe struct {
+	service Service
+}
+
+func (s *grpcServe) PostProduct(ctx context.Context, r *pb.PostProductRequest) (*pb.PostProductResponse, error) {
+	p, err := s.service.PostProduct(ctx, r.Name, r.Description, r.Price)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return &pb.PostProductResponse{Product: &pb.Product{
+		Id:          p.ID,
+		Name:        p.Name,
+		Description: p.Description,
+		Price:       p.Price,
+	}}, nil
+}
+
+func (s *grpcServe) GetProduct(ctx context.Context, r *pb.GetProductRequest) (*pb.GetProductResponse, error) {
+	p, err := s.service.GetProduct(ctx, r.Id)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return &pb.GetProductResponse{
+		Product: &pb.Product{
+			Id:          p.ID,
+			Name:        p.Name,
+			Description: p.Description,
+			Price:       p.Price,
+		},
+	}, nil
+}
+
+func (s *grpcServe) GetProducts(ctx context.Context, r *pb.GetProductsRequest) (*pb.GetProductsResponse, error) {
+	var res []Product
+	var err error
+	if r.Query != "" {
+		res, err = s.service.SearchProducts(ctx, r.Query, r.Skip, r.Take)
+	} else if len(r.Ids) != 0 {
+		res, err = s.service.GetProductsByIDs(ctx, r.Ids)
+	} else {
+		res, err = s.service.GetProducts(ctx, r.Skip, r.Take)
+	}
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	products := []Product{}
+	for _, p := range res {
+		products = append(products,
+			&pb.Product{
+				Id:          p.ID,
+				Name:        p.Name,
+				Description: p.Description,
+				Price:       p.Price,
+			})
+	}
+	return &pb.GetProductsResponse{Products: products}, nil
+}
+
+func ListenGRPC(s Service, port int) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return err
+	}
+	serv := grpc.NewServer()
+	pb.RegisterCatalogServiceServer(serv, &grpcServe{s})
+	reflection.Register(serv)
+	return serv.Serve(lis)
+}
